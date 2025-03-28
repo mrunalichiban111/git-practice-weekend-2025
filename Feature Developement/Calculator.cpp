1ae600420dcb7732401d19d1768117aab9c1f746
Mrunal Kulkarni <mrunalkul185@gmail.com>
2025-03-29 05:18:02 +0530

    added addition function

--- Feature Developement/Calculator.cpp
+++ Feature Developement/Calculator.cpp
@@ -2,3 +2,11 @@
 using namespace std;
 
 // This is a calculator program which does all basic arithmatic operations
+
+//so let's start 
+
+// 1st lets make a addition function 
+
+int add(int a,int b){
+    return a+b;
+}
