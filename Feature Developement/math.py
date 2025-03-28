3cab75d8cd958a847cdf052becfa7cf7a0d0b39d
Mrunal Kulkarni <mrunalkul185@gmail.com>
2025-03-29 05:12:51 +0530

    Added math.py file

---
+++ Feature Developement/math.py
@@ -0,0 +1,11 @@
+print("Diffrent Arithmatic operations are :")
+
+print("Addition => Operator used : +")
+
+print("Subtraction => Operator used : -")
+
+print("Multiplication => Operator used : *")
+
+print("Division => Operator used : /")
+
+print("Modulo => Operator used : %")
